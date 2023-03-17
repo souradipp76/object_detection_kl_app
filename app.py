a9bdaa322f70ce6a9feac915a012a3535dbe1dfb
souradip_pal <souradip.iitg@gmail.com>
2023-03-17 02:15:36 -0400

    Using gdrive id for download.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 import model
 
-model_url_dict = {
+model_id_dict = {
     'Faster-RCNN-Resnet50-FPN': '12HQmHq2KD52JjtLWYTFcDShMl-a7xlE4', 
     'Faster-RCNN-Resnet50-FPN + KL Loss': '1vOvWUTMH9D7diTo_mbhswkWOR7WnXe6N'
 }
@@ -57,24 +57,24 @@ def fix_image(upload, model_name):
 def get_detections(image, model_name):
     model_path = model_path_dict[model_name]
     cfg = model_conf_dict[model_name]
-    model_url = model_url_dict[model_name]
+    model_id = model_id_dict[model_name]
     if not os.path.isfile(model_path):
-        download_model(model_url, model_path)
+        download_model(model_id, model_path)
     net = model.get_model(model_path = model_path, cfg=cfg)
     fixed_image = model.get_sample_prediction(net, image)
     return fixed_image
 
 @st.cache()
-def download_model(model_url, model_path):
-    url = model_url
+def download_model(model_id, model_path):
+    id = model_id
     output = model_path
-    gdown.download(url, output, quiet=False)
+    gdown.download(id=id, output=output, quiet=False)
     return output
 
 
 col1, col2 = st.columns(2)
 my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-my_model = st.sidebar.selectbox("Choose Model", list(model_url_dict.keys()))
+my_model = st.sidebar.selectbox("Choose Model", list(model_id_dict.keys()))
 
 
 if my_upload is not None:
